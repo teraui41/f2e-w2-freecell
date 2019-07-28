e8fca6eaa0e7d25799bce057c36b89f31dfaad94
vick.wu <vick.wu.isap@gmail.com>
2019-07-29 00:55:30 +0800

    Updates

---
+++ precache-manifest.40e510b3170d39ea7f3ef49c3012b263.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316daf5599dc444668b3af3ad99c0096",
+    "url": "/f2e-w2-freecell/index.html"
+  },
+  {
+    "revision": "7ca146d3e15bb950817d",
+    "url": "/f2e-w2-freecell/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ad1f4e85db20888db81c",
+    "url": "/f2e-w2-freecell/static/js/2.4e019ef6.chunk.js"
+  },
+  {
+    "revision": "7ca146d3e15bb950817d",
+    "url": "/f2e-w2-freecell/static/js/main.2b409567.chunk.js"
+  },
+  {
+    "revision": "a61f2c53eb0dfd7317fc",
+    "url": "/f2e-w2-freecell/static/js/runtime~main.2e5c3c8b.js"
+  }
+]);
